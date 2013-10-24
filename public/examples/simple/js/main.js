acea67e97b0f2b8953c8311fa04328890efb044b
Patrick Brouwer <patrick@inlet.nl>
2013-10-24 11:50:08 +0200

    Update simple example

--- public/examples/simple/js/main.js
+++ public/examples/simple/js/main.js
@@ -3,20 +3,48 @@
  */
 
 
-var Animation = spirit.Timeline.extend({
+$(function() {
+	'use strict';
 
-	initialize: function() {
-		'use strict';
+	var jsonData = {
+		"elements": [
+			{
+				"id": "menu",
+				"transitions": [
+					{
+						"frame": 0,
+						"ease": "Linear.easeNone",
+						"params": [
+							{ "param": "translateX", "value": -50 },
+							{ "param": "translateY", "value": -400 }
+						]
+					},
+					{
+						"frame": 200,
+						"ease": "Cubic.easeOut",
+						"params": [
+							{ "param": "translateX", "value": 100 },
+							{ "param": "translateY", "value": 400 }
+						]
+					}
+				]
+			}
+		]
+	};
 
-		console.log('initialize timeline');
-	}
 
+	var Timeline = spirit.Timeline.extend({
+
+		jsonData: jsonData,
+
+		initialize: function() {
+			console.log('initialized timeline');
+		}
+	});
+
+
+	var tl = new Timeline($('#container'));
 
 });
-
-
-
-var tl = new Animation($('#container'));
-
 
 
