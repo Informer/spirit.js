9ee6f0a4cb607b3d1b733ac23279302295973ecb
Patrick Brouwer <patrick@inlet.nl>
2013-10-29 17:56:51 +0100

    Delegate namespaced collections & models in ElementModelSpec

--- test/ElementModelSpec.js
+++ test/ElementModelSpec.js
@@ -9,17 +9,21 @@ var _ = use('spirit._helpers');
 _.extend(_, use('jasmine._helpers'));
 
 
+var model = use('spirit.model'),
+	collection = use('spirit.collection');
+
+
 describe('ElementModel', function() {
 
 
 	var m;
 
 	it('should create a default model', function() {
-		m = new spirit.model.ElementModel();
+		m = new model.ElementModel();
 
 		expect(m.get('el')).toBeNull();
 		expect(m.get('id')).toBeNull();
-		expect(m.get('transitions') instanceof use('spirit.collection').TransitionCollection).toBeTruthy();
+		expect(m.get('transitions') instanceof collection.TransitionCollection).toBeTruthy();
 	});
 
 	it('should create a custom model', function() {
@@ -28,10 +32,10 @@ describe('ElementModel', function() {
 			id: 'element-1'
 		};
 
-		m = new spirit.model.ElementModel(props);
+		m = new model.ElementModel(props);
 		expect(m.get('el')).toBe(props.el);
 		expect(m.get('id')).toBe(props.id);
-		expect(m.get('transitions') instanceof use('spirit.collection').TransitionCollection).toBeTruthy();
+		expect(m.get('transitions') instanceof collection.TransitionCollection).toBeTruthy();
 	});
 
 
@@ -40,7 +44,7 @@ describe('ElementModel', function() {
 		var m;
 
 		beforeEach(function() {
-			m = new spirit.model.ElementModel({
+			m = new model.ElementModel({
 				el: $('<div />'),
 				id: 'element-1',
 				transitions: []
@@ -54,7 +58,7 @@ describe('ElementModel', function() {
 
 			expect(el instanceof $).toBeTruthy();
 			expect(id).toBe('element-1');
-			expect(transitions instanceof use('spirit.collection').TransitionCollection).toBeTruthy();
+			expect(transitions instanceof collection.TransitionCollection).toBeTruthy();
 		});
 
 		it('should store new values', function() {
