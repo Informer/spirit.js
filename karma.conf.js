cc238b3305fc82a8552a849135e0c6cb8594d482
Patrick Brouwer <patrick@inlet.nl>
2014-10-01 11:36:41 +0200

    Register with bower
    Plus clean up karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -2,63 +2,17 @@
 
 module.exports = function(config) {
 	config.set({
-
-		// base path, that will be used to resolve files and exclude
 		basePath: '',
-
-
-		// frameworks to use
 		frameworks: ['jasmine'],
-
-
-		// list of files / patterns to load in the browser
-		// Gruntfile.js handles this
 		files: [],
-
-
-		// list of files to exclude
 		exclude: [],
-
-
-		// test results reporter to use
-		// possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
 		reporters: ['progress'],
-
-
-		// web server port
 		port: 9999,
-
-
-		// enable / disable colors in the output (reporters and logs)
 		colors: true,
-
-
-		// level of logging
-		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
-
-
-		// enable / disable watching file and executing tests whenever any file changes
 		autoWatch: false,
-
-
-		// Start these browsers, currently available:
-		// - Chrome
-		// - ChromeCanary
-		// - Firefox
-		// - Opera
-		// - Safari (only Mac)
-		// - PhantomJS
-		// - IE (only Windows)
 		browsers: ['Chrome'],
-
-
-		// If browser does not capture in given timeout [ms], kill it
 		captureTimeout: 5000,
-
-
-		// Continuous Integration mode
-		// if true, it capture browsers, run tests and exit
 		singleRun: true
 	});
 };
