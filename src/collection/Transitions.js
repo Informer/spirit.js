2debf99f35a8e223650e03aad15e5f57e88de635
Patrick Brouwer <patrick@inlet.nl>
2014-10-03 11:37:06 +0200

    Automatically order models in Transitions by frame and pass each model as list

--- src/collection/Transitions.js
+++ src/collection/Transitions.js
@@ -1,25 +1,60 @@
 (function(ns) {
 
-	'use strict';
+  'use strict';
 
-	ns.Transitions = ns.Abstract.extend({
+  ns.Transitions = ns.Abstract.extend({
 
-		model: 'spirit.model.Transition',
+    model: 'spirit.model.Transition',
 
-		initialize: function() {
-			// make sure each model has always a reference to it's previous one
-			this.on('add', this.added);
-			this.on('remove', this.removed);
-		},
+    /**
+     * Regex mappings
+     * Evaluated values are piped through these mappings first
+     */
+    mappings: [],
+    comparator: 'frame',
 
-		added: function() {
-//			console.log('TransitionCollection -> added model');
-		},
+    /**
+     * Initialize Collection of model.Transition
+     * @returns {ns.Transitions}
+     */
+    initialize: function() {
+      _.autoBind(this);
+      this.mappings = [];
 
-		removed: function(){
-//			console.log('TransitionCollection -> removed model');
-		}
+      // make sure each model has always a reference to it's previous one
+      this.on('add remove', this.updateAll);
+      return this;
+    },
 
-	});
+    /**
+     * Update a single model with previousModel
+     * @param m
+     * @param coll
+     * @param options
+     * @returns {ns.Transitions}
+     */
+    updateModel: function(m, coll, options){
+      var index = this.indexOf(m),
+          modelAbove;
+
+      if (index > 0) {
+        modelAbove = this.at(index - 1);
+      }
+      m.set({previousModel: modelAbove});
+      return this;
+    },
+
+    /**
+     * Update all models with previousModel
+     * @param m
+     * @param coll
+     * @param options
+     * @returns {ns.Transitions}
+     */
+    updateAll: function(){
+      return this.each(this.updateModel);
+    }
+
+  });
 
 })(use('spirit.collection'));
--- test/collection/TransitionsSpec.js
+++ test/collection/TransitionsSpec.js
@@ -1,65 +1,89 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	var _ = jasmine._helpers;
+  var _ = jasmine._helpers;
 
-	describe('collection.Transitions', function() {
+  describe('collection.Transitions', function() {
 
-		var coll,
-			transitions = _.loadFixture('transitions.json').transitions;
-
-
-		it('should create a default collection', function() {
-			coll = new spirit.collection.Transitions();
-			expect(coll instanceof spirit.collection.Transitions).toBeTruthy();
-		});
+    var coll,
+        transitions = _.loadFixture('transitions.json').transitions;
 
 
-		describe('Create custom collection', function() {
+    it('should create a default collection', function() {
+      coll = new spirit.collection.Transitions();
+      expect(coll instanceof spirit.collection.Transitions).toBeTruthy();
+    });
 
-			beforeEach(function() {
-				coll = new spirit.collection.Transitions(transitions);
-			});
 
-			afterEach(function() {
-				coll = null;
-			});
+    describe('Create custom collection', function() {
 
-			it('should have 2 transitions', function() {
-				expect(coll.length).toEqual(2);
-			});
+      beforeEach(function() {
+        coll = new spirit.collection.Transitions(transitions);
+      });
 
-			it('should have 2 models of type model.Transition', function() {
-				_.each(coll.models, function(m) {
-					expect(m instanceof spirit.model.Transition);
-				});
-			});
+      afterEach(function() {
+        coll = null;
+      });
 
-			it('should have 1 model after deleting 1st model', function() {
-				coll.remove(coll.models[0]);
-				expect(coll.length).toEqual(1);
+      it('should have 2 transitions', function() {
+        expect(coll.length).toEqual(2);
+      });
 
-				var m = coll.models[0];
-				expect(m.get('frame')).toEqual(200);
-			});
+      it('should have 2 models of type model.Transition', function() {
+        _.each(coll.models, function(m) {
+          expect(m instanceof spirit.model.Transition);
+        });
+      });
 
-			it('should have 3 models after adding another', function() {
-				var m = new spirit.model.Transition({
-					params: {},
-					ease: 'Linear.awesomeness'
-				});
+      it('should have 1 model after deleting 1st model', function() {
+        coll.remove(coll.models[0]);
+        expect(coll.length).toEqual(1);
 
-				var added = coll.add({
-					params: {},
-					ease: 'Linear.awesomeness'
-				});
-				expect(coll.length).toEqual(3);
-				expect(added.attributes).toEqual(m.attributes);
-				expect(added instanceof m.constructor).toBeTruthy();
-			});
+        var m = coll.models[0];
+        expect(m.get('frame')).toEqual(200);
+      });
 
-		});
+      it('should have 3 models after adding another', function() {
+        var m = new spirit.model.Transition({frame: 1, params: {}, ease: 'Linear.awesomeness'}),
+            added = coll.add({ frame: 2, params: {}, ease: 'Linear.awesomeness' });
 
-	});
+        expect(coll.length).toEqual(3);
+        expect(added instanceof m.constructor).toBeTruthy();
+      });
+
+    });
+
+    describe('Chaining models', function(){
+
+      var coll;
+
+      beforeEach(function(){
+        coll = new spirit.collection.Transitions();
+        coll.add({ frame: 100, ease: 'Linear.easeNone', params: [] });
+        coll.add({ frame: 12, ease: 'Linear.easeNone', params: [] });
+        coll.add({ frame: 323, ease: 'Linear.easeNone', params: [] });
+        coll.add({ frame: 33, ease: 'Linear.easeNone', params: [] });
+      });
+
+      afterEach(function(){
+        coll.reset();
+      });
+
+      it('should order collection by frame (via comparator)', function() {
+        expect(coll.at(0).get('frame')).toBe(12);
+        expect(coll.at(coll.length - 1).get('frame')).toBe(323);
+        expect(coll.pluck('frame')).toEqual([12, 33, 100, 323]);
+      });
+
+      it ('should have previous model', function(){
+        expect(coll.at(0).get('previousModel')).toBeUndefined();
+
+        _.times(coll.length-1, function(i){
+          expect(coll.at(i+1).get('previousModel')).toBe(coll.at(i));
+        });
+      });
+    });
+
+  });
 
 })();
