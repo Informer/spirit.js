effbb98a598d906d2393fa38a4cc05c18971c503
Patrick Brouwer <patrick@inlet.nl>
2014-10-03 13:19:08 +0200

    Create Timeline factory.
    Constructs a valid GASP timeline out of transitions of an element

--- src/factory/Timeline.js
+++ src/factory/Timeline.js
@@ -4,11 +4,35 @@
 
   /**
    * Construct a GSAP timeline
-   * @param transitions spirit.collection.TransitionCollection
+   * @param $el element to apply on
+   * @param transitions spirit.collection.Transitions
    * @returns {Timeline(Lite/Max)}
    */
-  ns.Timeline = function Timeline(transitions){
+  ns.Timeline = function Timeline($el, transitions){
+    var timeline = new globalDefaults.timeline({ useFrames: true, paused: true }),
+        mappings = transitions.mappings;
 
+    transitions.each(function(transition){
+
+      var frame = transition.get('frame'),
+          prevModel = transition.get('previousModel'),
+          prevFrame = prevModel ? prevModel.get('frame') : 0,
+          params = transition.get('params').constructTweenObject(mappings);
+
+      params.ease = transition.get('ease');
+
+      if (prevModel) {
+        // shift in time when prevModel is available
+        frame -= prevModel.get('frame');
+      }
+
+      if (!_.isEmpty(params)) {
+        timeline.add(globalDefaults.tween.to($el, frame, params), prevFrame);
+      }
+
+    }, this);
+
+    return timeline;
   };
 
 })(use('spirit.factory'));
---
+++ test/factory/TimelineSpec.js
@@ -0,0 +1,53 @@
+(function() {
+
+  'use strict';
+
+  var _ = jasmine._helpers;
+
+  describe('factory.Timeline', function() {
+
+    var data,
+        transitions;
+
+    beforeEach(function() {
+      data = _.loadFixture('transitions.json').transitions;
+      transitions = new spirit.collection.Transitions(data);
+    });
+
+    it('should create a valid GSAP timeline', function() {
+      var $el = $('<div/>'),
+          timeline = new spirit.factory.Timeline($el, transitions);
+
+      expect(timeline instanceof TimelineLite).toBeTruthy();
+      expect(timeline.duration()).toBe(transitions.max(function(m){ return m.get('frame');}).get('frame'));
+
+      // interpolate
+      timeline.seek(100);
+
+      expect(Math.floor($el.get(0)._gsTransform.x)).toBe(25);
+      expect(Math.floor($el.get(0)._gsTransform.y)).toBe(0);
+    });
+
+    it ('should create a valid GSAP timeline with evaluated $this mapping', function(){
+
+      var $el = $('<div style="width: 100px; height: 100px" data-spirit-id="element1" />'),
+          model = new spirit.model.Timeline({el: $el, transitions: data});
+
+      // add evaluated params
+      model.get('transitions').add({
+        frame: 300,
+        params: [{ param: 'x', value: '{-($this.height()*2)}'}]
+      });
+
+      // create timeline and advance to 300 frames
+      var timeline = new spirit.factory.Timeline(model.get('el'), model.get('transitions'));
+      timeline.seek(300);
+
+      expect($el.width()).toBe(100);
+      expect($el.height()).toBe(100);
+      expect($el.get(0)._gsTransform.x).toBe(-200);
+    });
+
+  });
+
+})();
