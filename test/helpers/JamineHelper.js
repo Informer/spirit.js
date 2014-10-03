1d923d30b7ba5f3f71aa15bc7c6524bd4fbc2fdc
Patrick Brouwer <patrick@inlet.nl>
2014-10-03 11:40:27 +0200

    Refactor JasmineHelper

--- test/helpers/JamineHelper.js
+++ test/helpers/JamineHelper.js
@@ -5,44 +5,38 @@
 var jasmine = window.jasmine || {};
 
 
-(function(){
-	'use strict';
+(function() {
+  'use strict';
 
-	/**
-	 * Extend with spirit helpers (lodash etc)
-	 */
-	var _ = spirit._helpers;
-	jasmine._helpers = jasmine._helpers || _.extend({}, _);
+  /**
+   * Extend with spirit helpers (lodash etc)
+   */
+  var _ = spirit._helpers;
+  jasmine._helpers = jasmine._helpers || _.extend({}, _);
 
+  (function(ns) {
 
+    'use strict';
 
-	(function(ns){
+    /**
+     * Loads fixture sync
+     * @param path
+     * @returns {{}} JSON data
+     */
+    ns.loadFixture = function(path) {
+      var fixture;
+      $.ajax({
+        async: false, // must be synchronous to guarantee that no tests are run before fixture is loaded
+        dataType: 'json',
+        url: 'base/test/fixtures/' + path,
+        success: function(data) {
+          fixture = (typeof data === 'string') ? JSON.parse(data) : data;
+        }
+      });
+      return fixture;
+    };
 
-		'use strict';
-
-
-		/**
-		 * Loads fixture sync
-		 * @param path
-		 * @returns {{}} JSON data
-		 */
-		ns.loadFixture = function(path){
-
-			var fixture;
-
-			$.ajax({
-				async: false, // must be synchronous to guarantee that no tests are run before fixture is loaded
-				dataType: 'json',
-				url: 'base/test/fixtures/' + path,
-				success: function(data) {
-					fixture = (typeof data === 'string') ? JSON.parse(data) : data;
-				}
-			});
-
-			return fixture;
-		};
-
-	})(jasmine._helpers);
+  })(jasmine._helpers);
 })();
 
 
