8ef45c6b442798abdc22fd7d65262d195fbb4775
Patrick Brouwer <patrick@inlet.nl>
2015-02-03 11:20:25 +0100

    Notify Chrome Extension when spirit is loaded
    + Expose spiritLoaded on window
    + Dispatch `spirit-loaded` event on window

--- src/footer.js
+++ src/footer.js
@@ -1,4 +1,23 @@
 
+  /**
+   * Expose spiritLoaded on window and dispatch event
+   * This way we make sure that the chrome extension gets notified
+   */
+  var dispatchLoadedEvent = function(ins){
+    window.spiritLoaded = true;
+    var loadEvt;
+    if (_.isFunction(window.CustomEvent)) {
+      loadEvt = new CustomEvent('spirit-loaded', {detail: ins});
+    }else{
+      loadEvt = document.createEvent('Event');
+      loadEvt.initEvent('spirit-loaded', true, true);
+      loadEvt.detail = ins;
+    }
+    if (loadEvt) {
+      window.dispatchEvent(loadEvt);
+    }
+  };
+
 	/**
 	 * Finally make spirit namespace available
 	 * if we're running in AMD environment return context as module
@@ -6,10 +25,13 @@
 	 */
 	if (typeof define === "function" && define.amd) {
 		define(function() {
-			return context.spirit || {};
+      var ins = context.spirit || {};
+      dispatchLoadedEvent(ins);
+			return ins;
 		});
 	}else {
 		_.extend(global, context);
+    dispatchLoadedEvent(context.spirit);
 	}
 
 })(window);
--- test/amd/AMDSpec.js
+++ test/amd/AMDSpec.js
@@ -1,31 +1,69 @@
-define(['spirit.min'], function(spirit) {
+define(['jquery'], function($) {
 
   'use strict';
 
+  var spirit;
+
+  var waitForSpirit = function() {
+    spirit = false;
+    require(['spirit.min'], function(s) { spirit = s; });
+    waitsFor(function() { return !!spirit; }, 'Timed out', 1000);
+  };
+
+  describe('chrome extension events', function() {
+    it('should trigger an event on spirit loaded', function() {
+      expect(window.spiritLoaded).toBeUndefined();
+
+      var responder = {event: function() {}};
+      spyOn(responder, 'event').andCallThrough();
+      expect(responder.event).not.toHaveBeenCalled();
+      $(window).on('spirit-loaded', responder.event);
+
+      waitForSpirit();
+      runs(function() {
+        expect(window.spiritLoaded).toBeTruthy();
+        expect(responder.event).toHaveBeenCalled();
+        expect(responder.event.calls.length).toBe(1);
+      });
+    });
+  });
+
   describe('AMD compatible', function() {
 
+    beforeEach(waitForSpirit);
+    afterEach(function() {
+      spirit = undefined;
+      window.spiritLoaded = undefined;
+    });
+
     it('should have a module of spirit', function() {
-      expect(spirit).toBeDefined();
+      runs(function() {
+        expect(spirit).toBeDefined();
+      });
     });
 
     it('should not be populated to global namespace (window)', function() {
-      expect(window.spirit).toBeUndefined();
+      runs(function() {
+        expect(window.spirit).toBeUndefined();
+        expect(spirit).toBeDefined();
+      });
     });
 
     it('should have API available', function() {
-      var data = [
-        { name: 'group 1', timelines: [] },
-        { name: 'group 2', timelines: [] },
-        { name: 'group 3', timelines: [] }
-      ];
+      runs(function() {
+        var data = [
+          {name: 'group 1', timelines: []},
+          {name: 'group 2', timelines: []},
+          {name: 'group 3', timelines: []}
+        ];
 
-      spirit.load(data);
+        spirit.load(data);
 
-      expect(spirit.groups.length).toBe(3);
-      expect(spirit.groups.get('group 2').timeline.duration()).toBe(0);
-      expect(spirit.toJSON()).toEqual(data);
+        expect(spirit.groups.length).toBe(3);
+        expect(spirit.groups.get('group 2').timeline.duration()).toBe(0);
+        expect(spirit.toJSON()).toEqual(data);
+      });
     });
-
   });
 
 });
